19e39f37649f9617046bfb327bfe349e9d61cecd
ali.rostamian <alibalii2000@gmail.com>
2020-12-29 03:46:15 +0330

    testing convert method with input with spaces

---
+++ src/test/java/StringToIntegerTest.java
@@ -0,0 +1,28 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class StringToIntegerTest
+{
+    @Test
+    public void convertTest_Normal_Input() {
+        try {
+            assertEquals(1234, StringToInteger.convert("1234"));
+            assertEquals(-1234,StringToInteger.convert("-1234"));
+        } catch (Exception e) {
+            Assert.fail();
+        }
+    }
+    @Test
+    public void convertTest_InputWithSpace() {
+        try {
+            assertEquals(1234, StringToInteger.convert("1 5"));
+        } catch (Exception e) {
+            Assert.fail();
+        }
+    }
+}
